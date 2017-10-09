bb296da559a6ce07ea96b81c4aa664b81340b839
sdhnshu <sudhanshupassi@gmail.com>
2017-10-10 00:16:02 +0530

    progress!!!

--- img captioning/train.py
+++ img captioning/train.py
@@ -1,22 +1,39 @@
-# import torchvision.datasets as dsets
-
-# train_dataset = dsets.CocoCaptions(root='../datasets/train2014',
-#                                    annFile='../datasets/annotations/captions_train2014.json')
+import torchvision.datasets as dsets
+from torchvision import transforms
+from collections import Counter
+import pickle
 from pycocotools.coco import COCO
+from model import EncoderCNN, DecoderRNN
 
-coco = COCO('../datasets/annotations/captions_train2014.json')
-imgids = coco.getImgIds()
-print('imgids: ', len(imgids))
-anns = []
-keyerrors = []
-for img in imgids:
-    try:
-        anns.append(coco.loadAnns(img))
-    except KeyError:
-        keyerrors.append(img)
+coco_cap = COCO('../datasets/annotations/captions_train2014.json')
+ann_ids = coco_cap.anns.keys()
 
-print('anns: ', len(anns))
-print('errors: ', len(keyerrors))
+counter = Counter()
+for i, id in enumerate(ann_ids):
+    caption = str(coco_cap.anns[id]['caption'])
+    counter.update(caption.lower().split())
+words = set([word for word, cnt in counter.items() if cnt >= 4])
+words.add('<pad>')
+words.add('<start>')
+words.add('<end>')
+words.add('<unk>')
 
-imgids = list(coco.anns.keys())
-print('anns keys', len(imgids))
+word_to_int = {w: i for i, w in enumerate(words)}
+int_to_word = dict(enumerate(words))
+with open('../trained/img_cap_vocab.pkl', 'wb+') as f:
+    pickle.dump((word_to_int, int_to_word), f)
+
+train_dataset = dsets.CocoCaptions(root='../datasets/train2014',
+                                   annFile='../datasets/annotations/captions_train2014.json')
+
+transform = transforms.Compose([transforms.RandomCrop(224),
+                                transforms.RandomHorizontalFlip(),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.485, 0.456, 0.406),
+                                                     (0.229, 0.224, 0.225))])
+
+encoder = EncoderCNN(256)
+decoder = DecoderRNN(256, 512, len(int_to_word), 1)
+
+encoder.cuda()
+decoder.cuda()
